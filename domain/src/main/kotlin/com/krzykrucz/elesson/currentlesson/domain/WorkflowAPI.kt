2bed1f0c62c793547fd11cbdb38d30dfaf7e4da5
k.kruczynski <chris.kruczynski1@gmail.com>
2019-08-02 21:51:28 +0200

    starting lesson workflows + strict typing

--- domain/src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/WorkflowAPI.kt
+++ domain/src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/WorkflowAPI.kt
@@ -1,7 +1,10 @@
 package com.krzykrucz.elesson.currentlesson.domain
 
+import arrow.core.Option
+import arrow.effects.IO
+import java.time.LocalDateTime
 
-sealed class Result {
-    object Success : Result()
-    object Failure : Result()
-}
+
+typealias CheckScheduledLesson = (Teacher, LocalDateTime) -> IO<ScheduledLesson>
+typealias FetchClassRegistry = (ClassName) -> IO<ClassRegistry>
+typealias StartLesson = (FetchClassRegistry, CheckScheduledLesson, Teacher, LocalDateTime) -> Option<LessonIntroduction>
--- domain/src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/WorkflowImplementation.kt
+++ domain/src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/WorkflowImplementation.kt
@@ -1,2 +1,9 @@
 package com.krzykrucz.elesson.currentlesson.domain
 
+import arrow.core.Option
+
+fun startLesson(): StartLesson {
+    return { fetchClassRegistry, checkScheduledLesson, teacher, localDateTime ->
+        Option.empty()
+    }
+}

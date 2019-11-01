f0283ae6ef38af615bae2394cf4ffbff087d307c
FatCaterpillar <szymon.gasienica.kotelnicki@gmail.com>
2019-11-01 22:40:21 +0100

    FX instead of Monad Transformers

--- adapters/src/main/kotlin/com/krzykrucz/elesson/currentlesson/attendance/adapters/rest/AttendanceRouterConfig.kt
+++ adapters/src/main/kotlin/com/krzykrucz/elesson/currentlesson/attendance/adapters/rest/AttendanceRouterConfig.kt
@@ -1,28 +1,47 @@
 package com.krzykrucz.elesson.currentlesson.attendance.adapters.rest
 
-import com.krzykrucz.elesson.currentlesson.attendance.domain.FetchCheckedAttendance
-import com.krzykrucz.elesson.currentlesson.attendance.domain.FetchNotCompletedAttendanceAndRegistry
-import com.krzykrucz.elesson.currentlesson.attendance.domain.PersistAttendance
+import arrow.core.getOrElse
+import arrow.fx.IO
+import arrow.fx.typeclasses.Duration
+import com.krzykrucz.elesson.currentlesson.attendance.adapters.usecase.handleGetAttendanceRequest
+import com.krzykrucz.elesson.currentlesson.attendance.adapters.usecase.handleLateAttendanceDto
+import com.krzykrucz.elesson.currentlesson.attendance.adapters.usecase.handleNoteAbsentDto
+import com.krzykrucz.elesson.currentlesson.attendance.adapters.usecase.handleNotePresentDto
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyToMono
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
+import java.util.concurrent.TimeUnit
 
 // TODO divide this config to 3 different adapters
 @Configuration
 class AttendanceRouterConfig {
 
     @Bean
-    fun attendanceRouter(
-            persistAttendance: PersistAttendance,
-            fetchNotCompletedAttendance: FetchNotCompletedAttendanceAndRegistry,
-            fetchCheckedAttendance: FetchCheckedAttendance
-    ) = router {
+    fun attendanceRouter() = router {
         (path("/attendance") and accept(MediaType.APPLICATION_JSON)).nest {
-            POST("/absent", handleNoteAbsentRequest(persistAttendance, fetchNotCompletedAttendance))
-            POST("/present", handleNotePresentRequest(persistAttendance, fetchNotCompletedAttendance))
-            POST("/late", handleNoteLateRequest(persistAttendance, fetchCheckedAttendance))
+            POST("/absent") { request ->
+                request.toAttendanceDto()
+                    .flatMap { handleNoteAbsentDto(it).run() }
+            }
+            POST("/present") { request ->
+                request.toAttendanceDto()
+                    .flatMap { handleNotePresentDto(it).run() }
+            }
+            POST("/late") { request ->
+                request.bodyToMono<LateAttendanceDto>()
+                    .flatMap { handleLateAttendanceDto(it).run() }
+            }
             GET("", handleGetAttendanceRequest())
         }
     }
+
+    private fun ServerRequest.toAttendanceDto() = this.bodyToMono<AttendanceDto>()
+    private fun IO<Mono<ServerResponse>>.run(): Mono<ServerResponse> =
+        this.unsafeRunTimed(Duration(1, TimeUnit.SECONDS))
+            .getOrElse { ServerResponse.badRequest().build() }
 }

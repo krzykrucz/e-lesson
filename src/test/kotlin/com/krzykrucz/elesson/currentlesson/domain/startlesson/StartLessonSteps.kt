375901520c999a9af3c2026c3b3672cca0142a5d
krzykrucz <krzysiuk.66@gmail.com>
2019-11-25 14:34:33 +0100

    ex2

--- src/test/kotlin/com/krzykrucz/elesson/currentlesson/domain/startlesson/StartLessonSteps.kt
+++ src/test/kotlin/com/krzykrucz/elesson/currentlesson/domain/startlesson/StartLessonSteps.kt
@@ -12,17 +12,9 @@ class StartLessonSteps : En {
         }
         Given("Class registry for class {word}") { className: String ->
         }
-        Given("Failed to check lesson schedule") {
-        }
-        Given("Failed to fetch class registry") {
-        }
         When("Lesson is started at {word}") { startTime: String ->
         }
         Then("Lesson before attendance should be started") {
-        }
-        Then("Lesson should not be started because no scheduled lesson") {
-        }
-        Then("Lesson should not be started because class registry unavailable") {
         }
     }
 

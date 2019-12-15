579281f07564d307731193dccaba74042d94496b
konrad <mr.konrad.jakubiec@gmail.com>
2019-12-15 18:55:49 +0100

    #67 add db cleanup

--- src/test/groovy/com/krzykrucz/elesson/currentlesson/adapters/AcceptanceSpec.groovy
+++ src/test/groovy/com/krzykrucz/elesson/currentlesson/adapters/AcceptanceSpec.groovy
@@ -1,5 +1,6 @@
 package com.krzykrucz.elesson.currentlesson.adapters
 
+import com.krzykrucz.elesson.currentlesson.infrastructure.Database
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -9,5 +10,9 @@ import spock.lang.Specification
 class AcceptanceSpec extends Specification {
     @Autowired
     protected TestRestTemplate rest
+
+    def setup() {
+        Database.LESSON_DATABASE.clear()
+    }
 
 }

fc34130f9b828e905a6d3a7a7d03f8a0e1d850b9
krzykrucz <krzysiuk.66@gmail.com>
2020-05-21 00:28:26 +0200

    attendance in coroutines

--- src/main/kotlin/com/krzykrucz/elesson/currentlesson/adapters/CoroutineExtensions.kt
+++ src/main/kotlin/com/krzykrucz/elesson/currentlesson/adapters/CoroutineExtensions.kt
@@ -2,15 +2,9 @@ package com.krzykrucz.elesson.currentlesson.adapters
 
 import arrow.core.Either
 import arrow.core.EitherOf
-import arrow.core.extensions.either.traverse.sequence
 import arrow.core.fix
-import arrow.fx.IO
-import arrow.fx.extensions.io.applicative.applicative
-import arrow.fx.fix
-
-fun <A, B> Either<A, IO<B>>.sequence(): IO<Either<A, B>> =
-    this.sequence(IO.applicative()).fix()
-        .map { it.fix() }
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
 
 
 suspend fun <A, B, C> EitherOf<A, B>.asyncFlatMap(f: suspend (B) -> Either<A, C>): Either<A, C> =
@@ -29,3 +23,9 @@ suspend fun <A, B> EitherOf<A, B>.asyncDoIfRight(f: suspend (B) -> Unit): Either
         f(it)
         it
     }
+
+suspend fun <A, B> Either<A, B>.toServerResponse(): ServerResponse =
+    this.fold(
+        ifLeft = { ServerResponse.badRequest().bodyValueAndAwait(it as Any) },
+        ifRight = { ServerResponse.ok().bodyValueAndAwait(it as Any) }
+    )
--- src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/attendance/Persistence.kt
+++ src/main/kotlin/com/krzykrucz/elesson/currentlesson/domain/attendance/Persistence.kt
@@ -1,14 +1,13 @@
 package com.krzykrucz.elesson.currentlesson.domain.attendance
 
 import arrow.core.Option
-import arrow.fx.IO
 import com.krzykrucz.elesson.currentlesson.domain.shared.ClassRegistry
 import com.krzykrucz.elesson.currentlesson.domain.shared.LessonIdentifier
 
 
-typealias PersistAttendance = (LessonIdentifier, Attendance) -> IO<Boolean>
-typealias FetchCheckedAttendance = (LessonIdentifier) -> IO<Option<CheckedAttendanceList>>
-typealias FetchIncompleteAttendance = (LessonIdentifier) -> IO<Option<IncompleteAttendanceDto>>
+typealias PersistAttendance = suspend (LessonIdentifier, Attendance) -> Boolean
+typealias FetchCheckedAttendance = suspend (LessonIdentifier) -> Option<CheckedAttendanceList>
+typealias FetchIncompleteAttendance = suspend (LessonIdentifier) -> Option<IncompleteAttendanceDto>
 
 class IncompleteAttendanceDto(
     val incompleteAttendanceList: IncompleteAttendanceList,
